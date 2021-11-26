258fabc90ff4deaec1a257e8c2357ddc67845591
JK2K <ikuba03@gmail.com>
2021-11-26 21:37:13 +0100

    who cares

---
+++ main.js
@@ -0,0 +1,81 @@
+import express from "express"
+import {engine} from "express-handlebars"
+import path from "path"
+import formidableMiddleware from "express-formidable"
+import Fs from "@supercharge/filesystem"
+
+const app = express()
+const PORT = 3000;
+const reName = /(?:\.([^.]+))?$/;
+
+let files = []
+let id = 0;
+
+app.set('views', path.join(path.resolve(), 'views'));
+app.engine('hbs', engine({
+  defaultLayout: 'main.hbs',
+  extname: '.hbs',
+  partialsDir: 'views/partials',
+  helpers: {}
+}));
+app.set('view engine', 'hbs');
+
+app.use(formidableMiddleware({
+  uploadDir: path.join(path.resolve(), '/static/uploads/'),
+  multiples: true
+}))
+
+app.get('/', function (req, res) {
+  res.redirect('/upload')
+})
+app.get('/upload', function (req, res) {
+  res.render('upload.hbs', {
+    sideTitle: "multiupload",
+  })
+})
+app.post('/upload', async function (req, res) {
+  await Promise.all((Array.isArray(req.files.uploadedFiles) ? req.files.uploadedFiles: [req.files.uploadedFiles]).map(async (file) => {
+    file.id = id++
+    const pos  = file.path.search('/static')
+    file.path = file.path.substring(pos + 7)
+    const ext = reName.exec(file.name)
+
+    const fPatch = path.join(path.resolve(), `/static/gfx/${ext}.png`)
+
+    if(Fs.exists(fPatch)){
+      file.img = ext
+    }else {
+      file.img = '???'
+    }
+    files.push(file)
+  }))
+
+  res.redirect('/filemanager')
+})
+
+app.get('/filemanager', function (req, res) {
+  res.render('filemanager.hbs', {
+    sideTitle: "multiupload",
+    files: files,
+  })
+})
+
+
+app.get('/delete/:id', function (req, res) {
+  console.log(files, req.params.id)
+  files = files.filter(f => f.id !== parseInt(req.params.id))
+  res.redirect('/filemanager')
+})
+
+
+app.get('/info/:id', function (req, res) {
+
+})
+
+
+app.use(express.static('static'))
+
+app.listen(PORT, function () {
+  console.log("started at port: " + PORT)
+})
+
